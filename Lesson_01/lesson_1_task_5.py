0bd43754c76eb962969bdc92379a34b48ad2ff37
Konchenko <askon397@mail.ru>
2025-06-09 00:56:02 +0300

    Добавлены ответы на задания

---
+++ Lesson_01/lesson_1_task_5.py
@@ -0,0 +1,15 @@
+def num(x):
+    print(x, end='')
+
+
+print(8)
+print(8)
+print(0)
+print(0)
+print(5)
+print(5)
+print(5)
+print(3)
+print(5)
+print(3)
+print(5)
---
+++ Lesson_01/trening.py
@@ -0,0 +1,36 @@
+# Задание 1 из тренировки
+my_heigh = 179
+print(my_heigh)
+
+
+# Здание 2 из тренировки
+my_name = "Екатерина"
+my_name = "Екатерина Конченко"
+print(my_name)
+
+# Здание 3 из тренировки
+my_name = "Екатерина"
+my_name = "Екатерина Конченко"
+pet_name = input("Как зовут твоего питомца?")
+print(my_name)
+print("Ваш любимчик -" + pet_name)
+
+
+def print_python():
+    print("Учу Python!")
+
+
+print_python()
+
+
+def print_letter(let):
+    print(let, end='')
+
+
+print_letter('C')
+print_letter('т')
+print_letter('у')
+print_letter('д')
+print_letter('е')
+print_letter('н')
+print_letter('т')
