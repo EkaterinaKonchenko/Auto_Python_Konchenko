3421a0e72b07b82de3cd9c7e8f2ae4a786f74134
Konchenko <askon397@mail.ru>
2025-06-10 23:13:36 +0300

    на проверку

--- Lesson_04/test_string_utils.py
+++ Lesson_04/test_string_utils.py
@@ -15,6 +15,29 @@ def test_capitalize_positive(input_str, expected):
     assert string_utils.capitalize(input_str) == expected
 
 
+@pytest.mark.parametrize("str_input", ["    Lesson4"])
+def test_trim_positive(str_input):
+    assert string_utils.trim(str_input) == "Lesson4"
+
+
+@pytest.mark.parametrize("input_str, symbol, expected", [
+    ("skypro", "p", "skyro"),
+    ("hello world", "world", "hello "),
+    ("python", "th", "pyon"),
+])
+def test_delete_symbol_positive(input_str, symbol, expected):
+    assert string_utils.delete_symbol(input_str, symbol) == expected
+
+
+@pytest.mark.parametrize("input_str, symbol, expected", [
+    ("skypro", "p", True),
+    ("hello world", "world", True),
+    ("python", "sky", False),
+])
+def test_conyains_positive(input_str, symbol, expected):
+    assert string_utils.contains(input_str, symbol) == expected
+
+
 @pytest.mark.negative
 @pytest.mark.parametrize("input_str, expected", [
     ("123abc", "123abc"),
@@ -23,3 +46,26 @@ def test_capitalize_positive(input_str, expected):
 ])
 def test_capitalize_negative(input_str, expected):
     assert string_utils.capitalize(input_str) == expected
+
+
+@pytest.mark.parametrize("str_input", ["Lesson4      Lesson4"])
+def test_trim_negative(str_input):
+    assert string_utils.trim(str_input) == "Lesson4      Lesson4"
+
+
+@pytest.mark.parametrize("input_str, symbol, expected", [
+    ("skypro", "m", "skypro"),
+    ("123 world", "0", "123 world"),
+    ("_Python", "z", "_Python"),
+])
+def test_delete_symbol_negative(input_str, symbol, expected):
+    assert string_utils.delete_symbol(input_str, symbol) == expected
+
+
+@pytest.mark.parametrize("input_str, symbol, expected", [
+    ("123 skypro", "123", True),
+    (":-))", ")", True),
+    ("РУС#  ", " ", True),
+])
+def test_conyains_negative(input_str, symbol, expected):
+    assert string_utils.contains(input_str, symbol) == expected   
